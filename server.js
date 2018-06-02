38747ea7219346b1d76981c02d205339a749c830
Zuhnja Riekert <zuhnja@labs.epiuse.com>
2018-06-02 15:06:46 +0200

    Ran npm install --save graphql apollo-server-express

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
 
 const server = express();
+
+console.log("some text");
 
 server.listen(4000, () => {
     console.log("listening on port 4000");
